5e8f733f555d6befd0f598283c536fe58e9575d0
arbinzaman <arbinzmn@gmail.com>
2023-11-02 02:53:53 +0600

    Added location Api

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.get("/packages", (req, res) => {
     return res.json(data);
   });
 });
+
 app.get("/packagedetails", (req, res) => {
   const sqlSelect = "SELECT * FROM packagedetails";
   db.query(sqlSelect, (err, data) => {
@@ -48,6 +49,30 @@ app.get("/packagedetails", (req, res) => {
     return res.json(data);
   });
 });
+
+app.get("/packagedetails/location/dhaka", (req, res) => {
+  const sqlSelect = `SELECT * FROM packagedetails WHERE location = 'Dhaka';`;
+  db.query(sqlSelect, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/packagedetails/location/outsideDhaka", (req, res) => {
+  const sqlSelect = `SELECT * FROM packagedetails WHERE location = 'Outside Dhaka';`;
+  db.query(sqlSelect, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+app.get("/packagedetails/location/outdoor", (req, res) => {
+  const sqlSelect = `SELECT * FROM packagedetails WHERE location = 'Outdoor';`;
+  db.query(sqlSelect, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.get("/book", (req, res) => {
   const sqlSelect = "SELECT * FROM book";
   db.query(sqlSelect, (err, data) => {
@@ -62,13 +87,13 @@ app.post("/users", (req, res) => {
   const sql =
     "INSERT INTO users (userName,address,contact,email,password) VALUES (?)";
   const values = [
-    req.body.userName, 
-    req.body.address, 
-    req.body.contact, 
-    req.body.email, 
+    req.body.userName,
+    req.body.address,
+    req.body.contact,
+    req.body.email,
     req.body.password,
   ];
-console.log(values);
+  console.log(values);
   db.query(sql, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -79,48 +104,28 @@ app.post("/packagedetails", (req, res) => {
   const sql =
     "INSERT INTO packagedetails (title,price,description,location,event) VALUES (?)";
   const values = [
-    req.body.title, 
-    req.body.price, 
-    req.body.description, 
-    req.body.location, 
+    req.body.title,
+    req.body.price,
+    req.body.description,
+    req.body.location,
     req.body.event,
   ];
-console.log(values);
+  console.log(values);
   db.query(sql, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
 });
-
 
 app.post("/event", (req, res) => {
-  const sql =
-    "INSERT INTO event (title,description,img) VALUES (?)";
-  const values = [
-    req.body.title, 
-    req.body.description,
-    req.body.img,
-  ];
-console.log(values);
+  const sql = "INSERT INTO event (title,description,img) VALUES (?)";
+  const values = [req.body.title, req.body.description, req.body.img];
+  console.log(values);
   db.query(sql, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3001, () => {
   console.log("running on port 3001");
