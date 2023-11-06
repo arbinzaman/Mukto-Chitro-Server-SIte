ff9eba0db2d28856c534f3d722cb38f5eeb078de
arbinzaman <arbinzmn@gmail.com>
2023-11-06 14:30:01 +0600

    Added Update Admin Api

--- index.js
+++ index.js
@@ -85,17 +85,17 @@ async function main() {
 
     // app.get('/book/:id', (req, res) => {
     //   const bookingId = req.params.id; // Extract the booking ID from the URL
-    
+
     //   // Query the database to retrieve the booking by ID
     //   const query = 'SELECT * FROM book WHERE bookingId = ?';
-    
+
     //   db.query(query, [bookingId], (err, results) => {
     //     if (err) {
     //       console.error('Error querying the database:', err);
     //       res.status(500).json({ error: 'Internal Server Error' });
     //       return;
     //     }
-    
+
     //     if (results.length > 0) {
     //       const booking = results[0]; // Assuming there's only one result
     //       res.json(booking);
@@ -105,30 +105,28 @@ async function main() {
     //   });
     // });
 
-
-
-    app.get('/book/:customerEmail', (req, res) => {
+    app.get("/book/:customerEmail", (req, res) => {
       const customerEmail = req.params.customerEmail;
-    
-      const query = 'SELECT * FROM book WHERE customerEmail = ?';
-    
+
+      const query = "SELECT * FROM book WHERE customerEmail = ?";
+
       db.query(query, [customerEmail], (err, results) => {
         if (err) {
-          console.error('Error querying the database:', err);
-          res.status(500).json({ error: 'Internal Server Error' });
+          console.error("Error querying the database:", err);
+          res.status(500).json({ error: "Internal Server Error" });
         } else {
           if (results.length > 0) {
             res.json(results);
           } else {
-            res.status(404).json({ error: 'No book found for the given email' });
+            res
+              .status(404)
+              .json({ error: "No book found for the given email" });
           }
         }
       });
     });
 
-
-
-  // admin api
+    // admin api
     app.get("/users/admin/:email", (req, res) => {
       const email = req.params.email;
       const query = "SELECT role FROM users WHERE email = ?";
@@ -248,6 +246,19 @@ async function main() {
         }
       });
     });
+    app.delete("/book/:id", (req, res) => {
+      const { id } = req.params;
+      console.log(id);
+      const deleteQuery = "DELETE FROM book WHERE bookingID = ?";
+      db.query(deleteQuery, [id], (error, results) => {
+        if (error) {
+          console.error("Error deleting from the database: " + error);
+          res.status(500).json({ error: "Error deleting data" });
+        } else {
+          res.json({ message: "Row deleted successfully" });
+        }
+      });
+    });
 
     app.delete("/event/:id", (req, res) => {
       const { id } = req.params;
@@ -276,6 +287,24 @@ async function main() {
         } else {
           res.json({ message: "Row deleted successfully" });
         }
+      });
+    });
+
+    // Put Api
+
+    app.put("/users/:id", (req, res) => {
+      const id = req.params.id; 
+      console.log(id);
+      const newRole = "admin";
+      const updateQuery = "UPDATE users SET role = ? WHERE userID = ?";
+      db.query(updateQuery, [newRole, id], (err, result) => {
+        if (err) {
+          console.error("Error updating user role:", err);
+          res.status(500).json({ error: "Failed to update user role" });
+          return;
+        }
+        console.log("User role updated to admin");
+        res.json({ message: "User role updated to admin" });
       });
     });
 
